43b92bd5ad7c5d6ead788b117ccbb81ced7ac9a5
Yin <yinxiaoyu888@gmail.com>
2014-01-16 19:26:04 +0800

    显示阴影文字。

--- Hypnosister/HypnosisView.m
+++ Hypnosister/HypnosisView.m
@@ -37,6 +37,7 @@
     
     // 将线条颜色设为淡灰 (red/green/blue = 0.6, alpha = 1.0);
     [[UIColor lightGrayColor] setStroke];
+    
     // 按半径从大到小绘制多个同心圆
     for (float currentRadius = maxRadius; currentRadius > 0; currentRadius -= 20) {
         CGContextAddArc(ctx, center.x, center.y, currentRadius, 0.0, M_PI * 2.0, YES);
@@ -44,6 +45,40 @@
         // 执行绘图操作；移除路径
         CGContextStrokePath(ctx);
     }
+    
+    // 创建 NSString 对象
+    NSString *text = @"You are getting sleepy.";
+    
+    // 获取用于绘制字符串的字体
+    UIFont *font = [UIFont boldSystemFontOfSize:28];
+    
+    CGRect textRect;
+    
+    // 根据之前获取的字体，计算绘制字符串所需的图形大小
+    textRect.size = [text sizeWithAttributes:@{NSFontAttributeName: font}];
+    
+    // 准备将字符串绘制到视图正中
+    textRect.origin.x = center.x - textRect.size.width / 2.0;
+    textRect.origin.y = center.y - textRect.size.height / 2.0;
+    
+    // 将上下文的颜色填充为黑色
+    [[UIColor blackColor] setFill];
+    
+    // 根据字符串的绘制位置，创建阴影的偏移量，数值为向右偏 4 点，向下偏 3 点
+    CGSize offset = CGSizeMake(4, 3);
+    
+    // 阴影的颜色是深灰
+    CGColorRef color = [[UIColor darkGrayColor] CGColor];
+    
+    // 将以上得到的结构设置为当前上下文的阴影参数，后续的绘图操作都会附带阴影效果
+    CGContextSetShadowWithColor(ctx, offset, 2.0, color);
+    
+    // 绘制字符串
+    [text drawInRect:textRect withAttributes:@{NSFontAttributeName: font}];
+    
+
+    
+    
 }
 
 @end
