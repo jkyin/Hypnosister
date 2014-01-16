90a40c9cba74dfdaedce548f1ab5d9d43294f3ec
Yin <yinxiaoyu888@gmail.com>
2014-01-16 14:03:31 +0800

    Initial Commit

---
+++ Hypnosister/HypnosisterAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HypnosisterAppDelegate.h
+//  Hypnosister
+//
+//  Created by Yin on 14-1-16.
+//  Copyright (c) 2014年 Jack Yin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnosisterAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
